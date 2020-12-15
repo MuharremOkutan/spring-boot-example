f1e8808e2b6ba417498873ec3f67f339f2debc47
Muharrem Okutan <muharrem.okutan@gmx.at>
2020-12-15 21:12:43 +0100

    Account Test

--- spring.boot/src/main/java/com/tuturself/spring/boot/StudentSearchApplication.java
+++ spring.boot/src/main/java/com/tuturself/spring/boot/StudentSearchApplication.java
@@ -16,7 +16,7 @@ public class StudentSearchApplication {
 	/**
 	 * It is generally considered best practice to put exposed beans in
 	 * configuration classes, which are logically grouped.
-	 * 
+	 *
 	 * For example, you might have several configuration classes with a number
 	 * of beans contained within each. Here we create the bean in Main class
 	 * just for simplicity
@@ -25,4 +25,5 @@ public class StudentSearchApplication {
 	public StudentService studentService() {
 		return new StudentService();
 	}
+	//Account Test
 }
